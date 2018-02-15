59f4d27b4bcfbf9e7cc77f082fe14eb3850d3fcf
kurianbenoy <kurian.pro@gmail.com>
2018-02-16 00:09:16 +0530

    UI CHANGES on test branch

--- merathera.sh
+++ merathera.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-echo "                              STATUS                                                                           "
-echo "==================================================================================================================================================================="
+echo "                              STATUS "
+echo "==============================================================================="
 git status
-echo "==================================================================================================================================================================="
+echo "==============================================================================="
 
-echo "          "
 echo "  "
 echo " "
 
-echo "Enter option:"
-echo "1. Add a file "
-echo "2. Add all file "
-echo "3. Add all files , commit"
+echo "ENTER THE OPTION"
+echo "1. ADD A FILE "
+echo "2. ADD ALL FILES "
+echo "3. ADD ALL FILES and COMMIT"
 echo "4. ADD All files, COMMIT AND PUSH"
 echo "5.PUSH the branch"
+echo "6.FORCE PUSH THE branch"
 echo "6. PULL From origin"
 echo "7. ADD new branch"
 echo "8. ADD new remote"
@@ -63,19 +63,24 @@ then
   git push origin master
 fi
 
-if [ $input = '6' ]
+if [ $input = '5' ]
+then
+  git push origin master --force
+fi
+
+if [ $input = '7' ]
 then
   git pull --rebase origin master
 fi
 
-if [ $input = '7' ]
+if [ $input = '8' ]
 then
   echo "Name the new branch"
   read branch
   git checkout -b $branch
 fi
 
-if [ $input = '8' ]
+if [ $input = '9' ]
 then
   echo "Enter the remote name"
   read rname
