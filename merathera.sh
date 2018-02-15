e1de7811981c3c85415c861881cd8ffa85cf6d91
kurianbenoy <kurian.pro@gmail.com>
2018-02-16 00:03:00 +0530

    add some UI changes, why nothing is coming

--- merathera.sh
+++ merathera.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
-
+echo "                              STATUS                                                                           "
+echo "==================================================================================================================================================================="
 git status
+echo "==================================================================================================================================================================="
+
+echo "          "
+echo "  "
+echo " "
+
 echo "Enter option:"
 echo "1. Add a file "
 echo "2. Add all file "
 echo "3. Add all files , commit"
 echo "4. ADD All files, COMMIT AND PUSH"
-echo "5. PULL From origin"
-echo "6. ADD new branch"
-echo "7. ADD new remote"
+echo "5.PUSH the branch"
+echo "6. PULL From origin"
+echo "7. ADD new branch"
+echo "8. ADD new remote"
 
 
 read input
@@ -52,12 +60,27 @@ fi
 
 if [ $input = '5' ]
 then
-  git pull --rebase origin master
+  git push origin master
 fi
 
 if [ $input = '6' ]
 then
+  git pull --rebase origin master
+fi
+
+if [ $input = '7' ]
+then
   echo "Name the new branch"
   read branch
   git checkout -b $branch
+fi
+
+if [ $input = '8' ]
+then
+  echo "Enter the remote name"
+  read rname
+  echo "Add url"
+  read url
+
+  git remote add $rname $url
 fi
