8a9ce8f9ac6ef77efe96ff6073d57e0caf1dcfcf
Jeancarlo Hidalgo <jeancahu@gmail.com>
2018-02-18 13:03:59 -0600

    chg: Modified main code syntax

--- merathera.sh
+++ merathera.sh
@@ -1,91 +1,92 @@
 #!/bin/bash
-echo "                              STATUS "
-echo "==============================================================================="
+echo "                              STATUS
+==============================================================================="
 git status
-echo "==============================================================================="
-
-echo "  "
-echo " "
-
-echo "ENTER THE OPTION"
-echo "1. ADD A FILE "
-echo "2. ADD ALL FILES "
-echo "3. ADD ALL FILES and COMMIT"
-echo "4. ADD All files, COMMIT AND PUSH"
-echo "5.PUSH the branch"
-echo "6.FORCE PUSH THE branch"
-echo "6. PULL From origin"
-echo "7. ADD new branch"
-echo "8. ADD new remote"
-
-
-read input
-
-
-
-if [ $input = '1' ]
+if [ $? -ne 0 ]
 then
-  echo "Enter the file to add"
-  read file
-  git add $file
-  git status
-else
-  git add .
+    echo 'That is not a GIT repository'
+    exit 1
+fi
+echo "===============================================================================
+
+
+ENTER THE OPTION
+1. ADD A FILE
+2. ADD ALL FILES
+3. ADD ALL FILES and COMMIT
+4. ADD All files, COMMIT AND PUSH
+5. PUSH the branch
+6. FORCE PUSH THE branch
+6. PULL From origin
+7. ADD new branch
+8. ADD new remote"
+
+
+read INPUT
+
+if [ $INPUT -ne 1 ]
+then
+    git add .
 fi
 
+case $INPUT in
 
-if [ $input = '2' ]
-then
-  git add .
-  git status
-fi
+    1)
+	echo "Enter the file to add"
+	read FILE
+	git add $FILE
+	git status	
+	;;
 
-if [ $input = '3' ]
-then
-  git add .
-  echo "enter commit message"
-  read commit_message
-  git commit -m "$commit_message"
-fi
+    2)	
+	git status
+	;;
 
-if [ $input = '4' ]
-then
-  git add .
-  echo "enter commit message"
-  read commit_message
-  git commit -m "$commit_message"
-  git status
-  git push origin master
-fi
+    3)
+	echo "enter commit message"
+	read commit_message
+	git commit -m "$commit_message"
+	;;
 
-if [ $input = '5' ]
-then
-  git push origin master
-fi
+    4)
+	echo "enter commit message"
+	read commit_message
+	git commit -m "$commit_message"
+	git status
+	git push origin master
+	;;
 
-if [ $input = '6' ]
-then
-  git push origin master --force
-fi
+    5)
+	git push origin master
+	;;
 
-if [ $input = '7' ]
-then
-  git pull --rebase origin master
-fi
+    6)
+	git push origin master --force
+	;;
 
-if [ $input = '8' ]
-then
-  echo "Name the new branch"
-  read branch
-  git checkout -b $branch
-fi
+    7)
+	git pull --rebase origin master
+	;;
 
-if [ $input = '9' ]
-then
-  echo "Enter the remote name"
-  read rname
-  echo "Add url"
-  read url
+    8)
+	echo "Name the new branch"
+	read branch
+	git checkout -b $branch
+	;;
 
-  git remote add $rname $url
-fi
+    9)
+	echo "Enter the remote name"
+	read rname
+	echo "Add url"
+	read url
+	git remote add $rname $url
+	;;
+
+    *)
+	echo 'Sorry, you need to introduce one number listed'
+	exit 2
+	;;
+
+esac
+
+exit 0
